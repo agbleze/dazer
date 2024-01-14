6c4c66925c02508372961d5abbb8cb00f4bac04c
linus <lagbleze@st.ug.edu.gh>
2024-01-14 23:52:53 +0100

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot
 
 ## TO DO:
 # 1. create command line for train models and experimenting with different algorithms
+# 2. create a UI that visualize the model metrics for each model after training
 
 
 
