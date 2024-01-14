585bfa17c951c068cfbef509d02288c25489f437
linus <lagbleze@st.ug.edu.gh>
2024-01-15 00:15:25 +0100

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -13,8 +13,27 @@ from matplotlib import pyplot
 ## How package is expected to be used
 #1. developer preovides path of data and the target variable in the data.
 #2 has option to also specify column names of predictors to use. if not provided, all columns will be used
-#3. option to specify data type of the columns 
+#3. option to specify data type of the columns
+# option to define tabs to show on ui after training, mapping to change the names of the tabs with dict 
 #4. data format is csv for now,
+#5. the parameters to pass can defined in a config file of format json
+
+
+
+## UI 
+#1. features of the UI
+#2. page for visualizing graphs of models metric by comparing diff models
+#3. page for using model for prediction - contains inputs for selecting values for predictors used for modeling
+
+
+##  API 
+#1. The model should be packaged as a REST API
+#2. check to find free port for running the model api
+
+## OUTPUT OF running the package
+#1. Artefact
+#a. code of app ui for deployment
+#b. code for model api for deployment
 
 
 
